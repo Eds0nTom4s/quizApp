0c4ce393d14ab71623293ffc731853a4515f4aeb
Edson Tomas <edsonhacker13@gmail.com>
2023-09-17 01:14:26 +0100

    concluido a configuranção do reducer

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { useContext } from 'react'
+import '../components/Welcome.css';
 import { QuizContext } from '../context/quiz'
 
 import Quiz from '../img/quiz.svg'
 
 
 const Welcome = () => {
-	const quizState = useContext(QuizContext);
+	const [quizState,dispatch] = useContext(QuizContext);
 
-	console.log(quizState)
+	
   return (
 	<div className='welcome'>
 		<h2>Seja bem vindo</h2>
 		<p>Clique no botão abaixo para começar.</p>
-		<button>Iniciar</button>
+		<button onClick={()=>dispatch({type:"CHANGE_STATE"})}>Iniciar</button>
 		<img src={Quiz} alt="inicio do quiz" />
 	</div>
   )
--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -1,10 +1,27 @@
-import { createContext } from "react";
+import { createContext,useReducer} from "react";
+import questions from '../data/questions'
 
+const STAGES = ["Start","Playing","End"]
+
+const initialState = {
+	gameStage: STAGES[0],
+	questions
+}
+
+const quizReducer = (state,action)=>{
+	console.log(state,action)
+	switch(action.type){
+		case "CHANGE_STATE":
+			return state;
+		default:
+			return state;
+	}
+}
 //definição do contexto
 export const QuizContext = createContext();
 
 //definição do privider
 export const QuizProvider = ({children})=>{
-	const value = {nome:'Edson'}
+	const value = useReducer(quizReducer,initialState)
 	return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>
 }
